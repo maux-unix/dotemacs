818b997e039fbd56c89abe545ca182be7456cb1e
maux-unix <maulanalazim5@gmail.com>
2024-10-30 01:24:58 +0700

    first commit

---
+++ .emacs.custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(plan9-theme plan9-theme-theme company-c-headers zig-mode smex s rust-mode persistent-soft multiple-cursors move-text markdown-mode ido-completing-read+ ht gruber-darker-theme fireplace eglot-java company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
